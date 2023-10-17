de62448da5051f560539d83c3bbe2f5f88087d33
Daniel Florido <d.florido@javeriana.edu.co>
2023-10-17 18:36:53 -0500

    added models

--- src/app/model/piso/piso.ts
+++ src/app/model/piso/piso.ts
@@ -1,2 +1,12 @@
+import { Edificio } from "../edificio/edificio";
+import { Vehiculo } from "../vehiculo/vehiculo";
+
 export class Piso {
+    constructor(
+        public id: number|null,
+        public edificio: Edificio,
+        public totalVehiculos: number,
+        public espaciosDisponibles: number,
+        public vehiculos: Vehiculo[]
+    ){}
 }
--- src/app/model/vehiculo/tipo-vehiculo.ts
+++ src/app/model/vehiculo/tipo-vehiculo.ts
@@ -1,2 +1,11 @@
+import { Piso } from "../piso/piso";
+
 export class TipoVehiculo {
+    constructor(
+        public id : number|null,
+        public tipo: string,
+        public tarifa: number,
+        public area: number,
+        public pisos: Piso[]
+    ){}
 }
