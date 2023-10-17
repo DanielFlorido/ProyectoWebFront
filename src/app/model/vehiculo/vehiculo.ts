6fd1c5bf5fdd63477c57b320362b6409b5976d0c
Daniel Florido <d.florido@javeriana.edu.co>
2023-10-17 17:39:47 -0500

    added models

---
+++ src/app/model/vehiculo/vehiculo.ts
@@ -0,0 +1,11 @@
+import { TipoVehiculo } from './tipo-vehiculo';
+import { Piso } from './../piso/piso';
+export class Vehiculo {
+    constructor(
+        public id: number,
+        public placa: string,
+        public piso: Piso,
+        public tipoVehiculo: TipoVehiculo,
+        public horaLlegada: number
+    ){}
+}
