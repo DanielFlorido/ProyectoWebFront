210be2435d7ad92b9bf18c0b2ed03b2a0bdcd837
gams2004 <gabrielmartin2k4@gmail.com>
2023-10-17 23:45:32 -0500

    Prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Vehiculo } from './model/vehiculo/vehiculo';
 import { PisoService } from './shared/piso.service';
 import { Component } from '@angular/core';
 import { Edificio } from './model/edificio/edificio';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -13,11 +15,24 @@ import { Edificio } from './model/edificio/edificio';
 export class AppComponent {
   title = 'ProyectoWebFront';
 
-  constructor(private pisoService: PisoService) { }
+  constructor(private pisoService: PisoService, private formBuilder: FormBuilder) { }
+
+  checkoutForm = this.formBuilder.group({
+    placa: '',
+    pisoId: '',
+    tipoVehiculoS: '',
+    horaLlegada: ''
+  });
+
+  placaAux = (<HTMLInputElement>document.getElementById("placa")).value;
+  pisoIdAux = (<HTMLInputElement>document.getElementById("pisoId")).value;
+  tipoVehiculoSAux = (<HTMLInputElement>document.getElementById("tipoVehiculoS")).value;
+  horaLlegadaAux = (<HTMLInputElement>document.getElementById("horaLlegada")).value;
+
   edificio: Edificio = new Edificio();
-  piso: Piso = new Piso(1, this.edificio, 1, 1, null);
-  tipoVehiculo: TipoVehiculo = new TipoVehiculo(1, "", 1, 1, null);
-  vehiculo: Vehiculo = new Vehiculo(1, "", this.piso , this.tipoVehiculo, 1200)
+  piso: Piso = new Piso(parseInt(this.pisoIdAux), this.edificio, 1, 1, null);
+  tipoVehiculo: TipoVehiculo = new TipoVehiculo(1, this.tipoVehiculoSAux, 1, 1, null);
+  vehiculo: Vehiculo = new Vehiculo(1, this.placaAux, this.piso , this.tipoVehiculo, parseInt(this.horaLlegadaAux) )
 
   registrarVehiculo() {
     this.pisoService.registrarVehiculo(this.vehiculo).subscribe({
@@ -37,6 +52,14 @@ export class AppComponent {
         console.error(msg);
       }
     });
+  }
+
+  onSubmit(): void {
+    
+    // Process checkout data here
+    this.pisoService.crearVehiculo(this.vehiculo);
+    console.warn('Vehiculo registradp', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
