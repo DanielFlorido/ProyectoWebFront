452aa5be7c5cf1e082d750baf0a2cd35bac7e4b2
gams2004 <gabrielmartin2k4@gmail.com>
2023-11-19 22:22:40 -0500

    fix auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { VehiculoListComponent } from './vehiculo/vehiculo-list/vehiculo-list.component';
 import { VehiculoViewComponent } from './vehiculo/vehiculo-view/vehiculo-view.component';
 import { VehiculoRetirarComponent } from './vehiculo/vehiculo-retirar/vehiculo-retirar.component';
@@ -11,12 +10,14 @@ import { PisoListComponent } from './piso/piso-list/piso-list/piso-list.componen
 import { VehiculoListByPisoComponent } from './vehiculo/vehiculo-list-by-piso/vehiculo-list-by-piso/vehiculo-list-by-piso.component';
 import { HeaderComponent } from './header/header/header.component';
 import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { UsuarioCrearComponent } from './usuario/usuario-crear/usuario-crear/usuario-crear.component';
 import { FooterComponent } from './footer/footer/footer.component';
 import { HomeComponent } from './home/home/home.component';
 import { FAQComponent } from './FAQ/faq/faq.component';
 import { TipoVehiculoSliderComponent } from './tipo-vehiculo/tipo-vehiculo-slider/tipo-vehiculo-slider/tipo-vehiculo-slider.component';
 import { LoginComponent } from './auth/login/login.component';
+import { AuthInterceptor } from './helpers/auth.interceptor';
 
 
 @NgModule({
@@ -42,7 +43,9 @@ import { LoginComponent } from './auth/login/login.component';
     HttpClientModule,
     FormsModule 
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
